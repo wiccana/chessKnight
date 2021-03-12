6649ad1e840490aad5b646e952e07d4f8610e29c
brenda.mendez@sondeos.com.ar <brenda.mendez@sondeos.com.ar>
2021-03-12 17:07:42 -0300

    fix

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const challengeFunction = function(cell){
     /* YOUR CODE HERE */
-    let result
+    let letter = cell.split("")[0];
+    let number = cell.split("")[1];
+    let movs = 8;
+    
 
-    return result
+    if (letter == "A" || letter == "H"){
+       movs*= 0.5;
+    }else if(letter == "B" || letter == "G"){
+        movs*=0.75;
+    }
+    if (number == 1 || number == 8){
+        movs*=0.5;
+     }else if(number == 2 || number == 7){
+         movs*=0.75;
+     }
+     console.log(Math.floor(movs))
+    return Math.floor(movs)
 }
 
 module.exports = challengeFunction
